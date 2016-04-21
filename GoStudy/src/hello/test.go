4dc335d564863983171a915626257501d7d7983a
brynelee <bryne_lxd@sina.com>
2016-04-21 22:00:26 +0800

    GoStudy initialization.

---
+++ GoStudy/src/hello/test.go
@@ -0,0 +1,13 @@
+package main  
+  
+import (
+	"fmt" 
+	"newmath"
+)
+  
+func main() {  
+    fmt.Println("Hello world, I'm learning Golang")
+    fmt.Println("the square resuilt is %v",newmath.Sqrt(2))  
+}  
+
+
