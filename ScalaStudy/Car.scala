fd93c100d1f75dcabe0341e6934f64a97d9d1495
Xiaodong Li <bryne_lxd@sina.com>
2017-12-17 17:18:45 +0800

    add ScalaStudy part

---
+++ ScalaStudy/Car.scala
@@ -0,0 +1,14 @@
+class Car( val year: Int) { 
+
+	private var milesDriven: Int = 0 
+
+	def miles() = milesDriven 
+
+	def drive( distance: Int) { 
+
+		milesDriven += Math.abs(distance) 
+
+	} 
+
+}
+
