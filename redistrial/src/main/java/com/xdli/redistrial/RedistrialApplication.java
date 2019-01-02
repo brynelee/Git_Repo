d1ba8628a88b0c2c0cc9af1bb68ac552e3e0fa49
Xiaodong Li <bryne_lxd@sina.com>
2019-01-02 22:09:30 +0800

    Initial Spring Boot web application with simple test

---
+++ redistrial/src/main/java/com/xdli/redistrial/RedistrialApplication.java
@@ -0,0 +1,14 @@
+package com.xdli.redistrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedistrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedistrialApplication.class, args);
+    }
+
+}
+
